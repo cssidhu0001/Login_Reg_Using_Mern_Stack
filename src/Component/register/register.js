f15739b2c7b73d6bafc6cd646ccd2d26e2c0e9de
CHARAN_SIDHU <sidhucharan53@gmail.com>
2021-08-26 10:29:50 +0530

    image using  mutler

--- src/Component/register/register.js
+++ src/Component/register/register.js
@@ -3,6 +3,8 @@ import "./register.css";
 import axios from "axios";
 import {useHistory} from 'react-router-dom'
 
+
+
 const Register = ({settempuser}) => {
     const history =useHistory();
     const [user, setUser] = useState({
@@ -11,9 +13,15 @@ const Register = ({settempuser}) => {
         phone: "",
         address: "",
         gender: "",
+        passportimage:"",
         password: "",
         confirmpassword: ""
     })
+    const imageUpload=(e)=>{
+      setUser({...user,passportimage:e.target.passportimage
+      })
+    //   console.log(setUser)
+    }
 
     const handleChange = (e) => {
         const { name, value } = e.target
@@ -29,7 +37,7 @@ const Register = ({settempuser}) => {
         if (user){
             if(name && email && phone && address && gender && (password===confirmpassword)) {
                 axios.post("http://localhost:3400/emailexist",user).then(res=>{
-                    console.log(res.data.exist)
+                    // console.log(res.data.exist)
                     if (res.data.exist){
                         alert("User Already Registered..Kindly Login ")
                         history.push('/login')    
@@ -57,7 +65,7 @@ const Register = ({settempuser}) => {
             <h1 className='registerHeading'>Sign Up..! Create Account</h1>
             <h1 className='registerHeading1 ' >Already have a Account.<span onClick={loginuser}> Sign In</span>  </h1>
             <div className='registerformdiv'>
-                <form>
+                <form encType="multipart/form-data">
                     <label htmlFor='name' className='reigsterlabel'>Name:</label>
                     <input type='text' className='registerinput' id='name' name="name" value={user.name} onChange={handleChange} placeholder='Enter your name here..' required />
 
@@ -79,6 +87,10 @@ const Register = ({settempuser}) => {
 
                     <label htmlFor='address' className='reigsterlabel'>Address:</label>
                     <input type='textarea' rows='10' cols='5' className='registerinput' name="address" id='address' value={user.address} onChange={handleChange} placeholder='Enter your address here..' required />
+                    
+                    
+                    <label htmlFor='passportimage' className='reigsterlabel'>Passport Image:</label>
+                    <input type='file' className='registerinput' name="passportimage" id='passportimage'  onChange={imageUpload} required />
 
                     <label htmlFor='password' className='reigsterlabel'>Password:</label>
                     <input type='password' className='registerinput' id='password'  name="password" value={user.password}  onChange={handleChange}placeholder='Enter your password here..' required />
