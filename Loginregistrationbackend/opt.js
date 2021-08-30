3f3fc342433f1c450e4bd81535c32eebdb2307c9
CHARAN_SIDHU <sidhucharan53@gmail.com>
2021-08-30 13:08:55 +0530

    added log

--- Loginregistrationbackend/opt.js
+++ Loginregistrationbackend/opt.js
@@ -1,11 +1,25 @@
 import express from 'express';
 const app = express();
-import  randomize from 'randomatic';
- 
+// import  randomize from 'randomatic';
+import  nodeGeocoder  from 'node-geocoder'
 
 app.post("/otp",(req, res) => {
-const token = randomize("*",8)
-console.log(token);
+    let options = {
+        // provider: 'openstreetmap'
+        provider: 'google',
+        apiKey: process.env.LocationApiKey, 
+
+      };
+       
+      let geoCoder = nodeGeocoder(options);
+
+      geoCoder.geocode('haldwani')
+  .then((res)=> {
+    console.log(res);
+  })
+  .catch((err)=> {
+    console.log(err);
+  });
 })
 app.listen(3600, () => {
     console.log("server started at 3600")
