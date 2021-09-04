b6c3ed350591883bb93c3a77f2c80405109dbf26
CHARAN_SIDHU <sidhucharan53@gmail.com>
2021-09-04 08:47:27 +0530

    chnages

--- src/Component/homepage/homepage.js
+++ src/Component/homepage/homepage.js
@@ -1,8 +1,9 @@
 import React from "react"
 import "./homepage.css"
+import {useHistory } from 'react-router-dom';
 
 const Homepage=({user})=>{
-   
+    const history = useHistory();
     const logout = ()=>{
         user=undefined
         console.log("user-logout")
